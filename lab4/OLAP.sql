5dc2ab0643a0ce1bce4bc9827b4f3bc6b53185c2
sa1dake <deniskozhemyaka7@gmail.com>
2026-02-06 00:25:40 +0200

    Added .sql

--- lab4/OLAP.sql
+++ lab4/OLAP.sql
@@ -1,25 +1,13 @@
-# Лабораторна робота №4
-
-## Маніпулювання даними SQL (OLAP)
-
-### SQL-скрипт(и)
-
-
-```sql
 -- 1. Підрахунок загальної кількості відключень
 -- Аналіз загального навантаження на енергосистему
 SELECT COUNT(*) AS total_outages
 FROM outage_event;
-```
-![Alt text](images/total_outages.png)
-```sql
+
 -- 2. Обчислення середньої тривалості відключення
 -- Дозволяє оцінити середній час відсутності електропостачання
 SELECT AVG(end_datetime - start_datetime) AS avg_outage_duration
 FROM outage_event;
-```
-![Alt text](images/avg_outage.png)
-```sql
+
 -- 3. Кількість відключень для кожної групи живлення
 -- Аналіз стабільності роботи окремих груп
 SELECT pg.code AS power_group_code,
@@ -27,9 +15,7 @@ SELECT pg.code AS power_group_code,
 FROM power_group pg
 JOIN outage_event oe ON oe.group_id = pg.group_id
 GROUP BY pg.code;
-```
-![Alt text](images/outage_count_by_groups.png)
-```sql
+
 -- 4. Кількість відключень по районах
 -- Виявлення районів з найбільшою кількістю аварій
 SELECT d.name AS district_name,
@@ -38,17 +24,13 @@ FROM district d
 JOIN power_group pg ON pg.district_id = d.district_id
 JOIN outage_event oe ON oe.group_id = pg.group_id
 GROUP BY d.name;
-```
-![Alt text](images/outage_count_by_districts.png)
-```sql
--- additionally. Додавання фактичних відлючень для подальшого аналізу
+
+-- Додавання фактичних відлючень для подальшого аналізу
 INSERT INTO outage_event (group_id, start_datetime, end_datetime, reason)
 VALUES
 (1, '2025-02-15 08:00', '2025-02-15 10:00', 'scheduled'),
 (3, '2025-02-20 18:00', '2025-02-20 20:00', 'emergency');
-```
 
-```sql
 -- 5. Райони, де було більше одного відключення
 -- Фільтрація проблемних районів за допомогою HAVING
 SELECT d.name AS district_name,
@@ -58,9 +40,7 @@ JOIN power_group pg ON pg.district_id = d.district_id
 JOIN outage_event oe ON oe.group_id = pg.group_id
 GROUP BY d.name
 HAVING COUNT(oe.event_id) > 1;
-```
-![Alt text](images/districts_morethan1.png)
-```sql
+
 -- 6. Всі групи живлення та кількість відключень (LEFT JOIN)
 -- Включає групи, де відключень не було
 SELECT pg.code AS power_group_code,
@@ -68,18 +48,14 @@ SELECT pg.code AS power_group_code,
 FROM power_group pg
 LEFT JOIN outage_event oe ON oe.group_id = pg.group_id
 GROUP BY pg.code;
-```
-![Alt text](images/groups_and_outages.png)
-```sql
+
 -- 7. Групи живлення без жодного відключення
 -- Виявлення стабільних груп електропостачання
 SELECT pg.code AS power_group_code
 FROM power_group pg
 LEFT JOIN outage_event oe ON oe.group_id = pg.group_id
 WHERE oe.event_id IS NULL;
-```
-![Alt text](images/no_outages.png)
-```sql
+
 -- 8. Відключення, які тривали довше середнього значення
 -- Підзапит використовується для порівняння з середнім показником
 SELECT *
@@ -87,9 +63,7 @@ FROM outage_event
 WHERE (end_datetime - start_datetime) >
       (SELECT AVG(end_datetime - start_datetime)
        FROM outage_event);
-```
-![Alt text](images/long_outages.png)
-```sql
+
 -- 9. Район з максимальною кількістю відключень
 -- Підзапит використовується для пошуку найбільш проблемного району
 SELECT name
@@ -102,9 +76,7 @@ WHERE district_id = (
     ORDER BY COUNT(oe.event_id) DESC
     LIMIT 1
 );
-```
-![Alt text](images/depressed_disctrict.png)
-```sql
+
 -- 10. Кількість повідомлень за причинами відключень
 -- Аналіз типів відключень та активності інформування споживачів
 SELECT oe.reason,
@@ -112,5 +84,3 @@ SELECT oe.reason,
 FROM outage_event oe
 JOIN notification n ON n.event_id = oe.event_id
 GROUP BY oe.reason;
-```
-![Alt text](images/notification_count.png)
